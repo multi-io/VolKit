04f6fac8752d579cd47b844ef098a62fe4e02140
Olaf Klischat <olaf.klischat@gmail.com>
2014-03-31 19:38:04 +0200

    compile/run scripts: add newer dependencies

--- compile.sh
+++ compile.sh
@@ -11,11 +11,13 @@ utilcp="$commonbase/de.sofd.util/build/classes"
 swingcp="$commonbase/de.sofd.swing/build/classes"
 draw2dcp="$commonbase/de.sofd.draw2d/build/classes"
 viskitcp="$commonbase/de.sofd.viskit/build/classes"
+sofdtwlcp="$commonbase/de.olafklischat.twl/build/classes"
+sofdtwlawtcp="$commonbase/de.olafklischat.twlawt/bin"
 twlcp="$twlsrc/bin:`echo $twlsrc/elibs/*.jar | tr ' ' ':'`"
 
 selfcp="bin:`echo lib/*jar lib/jogl/*jar | tr ' ' ':'`"
 
-cp="$selfcp:$twlcp:$swingcp:$draw2dcp:$viskitcp:$utilcp"
+cp="$selfcp:$twlcp:$swingcp:$draw2dcp:$sofdtwlcp:$sofdtwlawtcp:$viskitcp:$utilcp"
 
 javac -cp "$cp" -d bin `find src -name '*java'`
 cp -f src/log4j.properties bin/
--- run.sh
+++ run.sh
@@ -11,11 +11,13 @@ utilcp="$commonbase/de.sofd.util/build/classes"
 swingcp="$commonbase/de.sofd.swing/build/classes"
 draw2dcp="$commonbase/de.sofd.draw2d/build/classes"
 viskitcp="$commonbase/de.sofd.viskit/build/classes"
+sofdtwlcp="$commonbase/de.olafklischat.twl/build/classes"
+sofdtwlawtcp="$commonbase/de.olafklischat.twlawt/bin"
 twlcp="$twlsrc/bin:`echo $twlsrc/elibs/*.jar | tr ' ' ':'`"
 
 selfcp="bin:`echo lib/*jar lib/jogl/*jar | tr ' ' ':'`"
 
-cp="$selfcp:$twlcp:$swingcp:$draw2dcp:$viskitcp:$utilcp"
+cp="$selfcp:$twlcp:$swingcp:$draw2dcp:$sofdtwlcp:$sofdtwlawtcp:$viskitcp:$utilcp"
 
 # jogl
 # java -Xmx8000m -Xms300m -Djava.library.path=lib/jogl/linux64 -Dno.jogl.verbose=true -Dno.jogl.debug=1 -cp "$cp" de/olafklischat/volkit/App
